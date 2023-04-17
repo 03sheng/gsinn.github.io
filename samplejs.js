36b925c7c4d24fc789a67814b584088113ea1cfa
03sheng <130943335+03sheng@users.noreply.github.com>
2023-04-17 16:35:22 +0800

    Add files via upload

---
+++ samplejs.js
@@ -0,0 +1,7 @@
+
+const buttonOne = document.getElementById("btn1").onclick = function(){
+    alert("You'll be redirected into another page ");};
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
